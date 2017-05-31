c98ba37a66fd719ab3909601cc763c426506cf94
sintell <a.anatskiy@hh.ru>
2017-05-31 19:45:37 +0300

    HH-69318 use new format of result reporting

--- gulp-qunit-mocha-report-file.js
+++ gulp-qunit-mocha-report-file.js
@@ -1,17 +1,15 @@
 'use strict';
 var fs = require('fs');
 
-module.exports = function(error, result, file) {
+module.exports = function(error, result) {
     if (result) {
         try {
-            result.trim().split('\n').forEach(function(line) {
-                if (line.indexOf('{') !== -1) {
-                    // Делаем JSON.parse, JSON.stringify чтобы убедиться, что пришел валидный JSON и его можно
-                    // писать в файл для дальнейшей обработки
-                    var report = JSON.parse(line.trim());
-                    fs.writeFileSync(`qunit-report-${file.relative}.json`, JSON.stringify(report), 'utf8');
-                }
-            });
+            if (line.indexOf('{') !== -1) {
+                // Делаем JSON.parse, JSON.stringify чтобы убедиться, что пришел валидный JSON и его можно
+                // писать в файл для дальнейшей обработки
+                var out = JSON.parse(result);
+                fs.writeFileSync(`qunit-report-${out.file}.json`, JSON.stringify(out.data), 'utf8');
+            }
         } catch (ignore) {}
     }
 };
